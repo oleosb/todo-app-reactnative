accfbb5634d6f209d640088e63c066621aa0476b
oleosb <oleosbo@gmail.com>
2023-11-13 11:01:17 -0300

    Middleware function

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,28 @@ import express from "express";
 // create a server
 const app = express();
 
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  // read the data and we want to add that to req.body
+  req.on("data", (chunk) => {
+    // manipulate thing here
+    req.body = JSON.parse(chunk);
+    next();
+  });
+});
+
 app.post("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  //here we need data so that we can create new note/todo
+  console.log(req.body);
+  res.json({ message: "I'm listening" });
+});
+
+app.post("/create", (req, res) => {
+  //here we need data so that we can create new note/todo
+  console.log(req.body);
+  res.json({ message: "I'm listening to create" });
 });
 
 // listen to some port
