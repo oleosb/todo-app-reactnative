22e320458db2406dead9c3c4aa966da9baa327c3
oleosb <oleosbo@gmail.com>
2023-11-15 08:18:01 -0300

    Submitting Form

--- server/frontend/src/App.tsx
+++ server/frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, ChangeEventHandler } from "react";
 import NoteItem from "./components/NoteItem";
+import axios from "axios";
 
 const App = () => {
   // const [title, setTitle] = useState("");
@@ -19,7 +20,20 @@ const App = () => {
 
   return (
     <div className="max-w-3xl mx-auto space-y-6">
-      <div className="space-y-4 bg-white shadow-md rounded p-5">
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          const { data } = await axios.post(
+            "http://localhost:8000/note/create",
+            {
+              title: values.title,
+              description: values.description,
+            }
+          );
+          console.log(data);
+        }}
+        className="space-y-4 bg-white shadow-md rounded p-5"
+      >
         <h1 className="font-semibold text-2xl text-center">Note Application</h1>
         <div>
           <input
@@ -41,14 +55,11 @@ const App = () => {
           ></textarea>
         </div>
         <div className="text-right">
-          <button
-            onClick={() => console.log(values)}
-            className="bg-blue-500 text-white px-5 py-2 rounded"
-          >
+          <button className="bg-blue-500 text-white px-5 py-2 rounded">
             Submit
           </button>
         </div>
-      </div>
+      </form>
 
       {/* Note Items */}
       <NoteItem title="Texto" />
