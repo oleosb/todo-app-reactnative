119dda3580cff2d5d9473ca168be8bd14c1c6a04
oleosb <oleosbo@gmail.com>
2023-11-15 06:14:08 -0300

    UI for note items

--- server/frontend/src/App.tsx
+++ server/frontend/src/App.tsx
@@ -1,12 +1,24 @@
 const App = () => {
   return (
-    <div className="w-[500px] mx-auto bg-gray-200">
-      <h1 className="font-semibold text-2xl text-blue-400">Note Application</h1>
+    <div className="max-w-3xl mx-auto bg-white shadow-md rounded p-5 space-y-4">
+      <h1 className="font-semibold text-2xl text-center">Note Application</h1>
       <div>
-        <input className="" type="text" placeholder="Title" />
+        <input
+          className="w-full border-b-2 border-gray-700 outline-none"
+          type="text"
+          placeholder="Title"
+        />
       </div>
       <div>
-        <textarea className="" placeholder="Description"></textarea>
+        <textarea
+          className="w-full border-b-2 border-gray-700 outline-none resize-none h-36"
+          placeholder="Description"
+        ></textarea>
+      </div>
+      <div className="text-center">
+        <button className="bg-blue-500 text-white px-5 py-2 rounded">
+          Submit
+        </button>
       </div>
     </div>
   );
